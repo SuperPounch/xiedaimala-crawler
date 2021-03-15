02989481fc3ddfa75c83a359cac49a2f81e83bb1
landlord <1240847431@qq.com>
2021-03-15 16:23:38 +0800

    Add SmokeTest.java

---
+++ src/test/java/SmokeTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author lx
+ * @create_TIME 2021-03-15-16:22
+ */
+public class SmokeTest {
+    @Test
+    public void test(){
+        //生成测试方法
+
+    }
+}
