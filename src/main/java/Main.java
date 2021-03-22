da49d36dc3e389d85951ba70bac91f5818e000fc
Lixiao <1240847431@qq.com>
2021-03-22 22:35:40 +0800

    设计并实现爬虫的基本算法 (#2)
    * Remove Comments
    
    * 设计并实现基本的算法
    
    这个提交中实现了基本的爬虫算法，虽然原始但首先进行小部分提交
    
    * 修复CI编译错误
    
    * 修复CI编译错误--2
    
    * 修复CI编译错误--3
    
    * 修复CI编译错误--4
    
    java 文件不以换行符结尾（checkstyle的规则）

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,20 +4,87 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import java.io.IOException;
-/**
- * @author lx
- * @create_TIME 2021-03-15-15:09
- */
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("https://sina.cn");
-            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                System.out.println(response1.getStatusLine());
-                HttpEntity entity1 = response1.getEntity();
-                System.out.println(EntityUtils.toString(entity1));
+        //待处理的链接池
+        List<String> linkpool = new ArrayList<>();
+        //已经处理的链接池
+        Set<String> processedlinks = new HashSet<>();
+        linkpool.add("https://sina.cn");
+
+        while (true) {
+            if (linkpool.isEmpty()) {
+                break;
+            }
+            //ArrayList 从尾部删除更效率
+            String link = linkpool.remove(linkpool.size() - 1);
+
+            if (processedlinks.contains(link)) {
+                continue;
+            }
+            // if (link.contains("sina.cn") && !link.contains("passport.sina.cn") && (link.contains("news.sina.cn") || "https://sina.cn".equals(link))) {
+            if (isInterestingLink(link)) {
+
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkpool::add);
+                storeIntoDataBaseIfItIsNewsPage(doc);
+                processedlinks.add(link);
             }
         }
+    }
+
+    private static void storeIntoDataBaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        //要处理的新浪站内链接，感兴趣
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36");
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
