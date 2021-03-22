ed362c4484ef74dad9dae428478371b96ec36ee8
landlord <1240847431@qq.com>
2021-03-22 21:29:03 +0800

    设计并实现基本的算法
    这个提交中实现了基本的爬虫算法，虽然原始但首先进行小部分提交

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,107 @@
 import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.jcp.xml.dsig.internal.dom.DOMCanonicalizationMethod;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import sun.plugin2.applet.viewer.util.AppletTagParser;
+
+import javax.print.Doc;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            HttpGet httpGet = new HttpGet("https://sina.cn");
-            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                System.out.println(response1.getStatusLine());
-                HttpEntity entity1 = response1.getEntity();
-                System.out.println(EntityUtils.toString(entity1));
+        //待处理的链接池
+        List<String> linkpool = new ArrayList<>();
+        //已经处理的链接池
+        Set<String> processedlinks = new HashSet<>();
+        linkpool.add("https://sina.cn");
+
+        while (true) {
+            if (linkpool.isEmpty()) {
+                break;
+            }
+            //ArrayList 从尾部删除更效率
+            String link = linkpool.remove(linkpool.size() - 1);
+
+            if (processedlinks.contains(link)) {
+                continue;
+            }
+            // if (link.contains("sina.cn") && !link.contains("passport.sina.cn") && (link.contains("news.sina.cn") || "https://sina.cn".equals(link))) {
+            if (isInterestingLink(link)) {
+
+                Document doc = httpGetAndParseHtml(link);
+                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkpool::add);
+                /*for (Element aTag : links) {
+                    linkpool.add(aTag.attr("href"));
+                }
+*/
+                //新闻的详情页面就保持，否则不做
+                storeIntoDataBaseIfItIsNewsPage(doc);
+
+                processedlinks.add(link);
+
+
             }
         }
+    }
+
+    private static void storeIntoDataBaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+        //要处理的新浪站内链接，感兴趣
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36");
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 }
