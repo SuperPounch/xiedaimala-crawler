664a18bb73a97211bacad97a4deef07da9a6d9e2
Lixiao <1240847431@qq.com>
2021-03-24 20:54:20 +0800

    Mac crawler (#4)
    *MAC IDEA 提交完成✅
    * Remove Comments
    
    * 设计并实现基本的算法
    
    这个提交中实现了基本的爬虫算法，虽然原始但首先进行小部分提交
    
    * 修复CI编译错误
    
    * 修复CI编译错误--2
    
    * 修复CI编译错误--3
    
    * 修复CI编译错误--4
    
    java 文件不以换行符结尾（checkstyle的规则）
    
    * Add H2 sql
    
    * Add H2 sql 2
    
    * H2 Finished
    
    使用H2数据库完成数据存储，并且对代码进行重构
    
    * Please enter the commit message for your changes. Lines starting
    
    Co-authored-by: SuperPounch <superpounch@github.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -8,22 +9,84 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static List<String> loadUrlsFromDataBase(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return results;
+    }
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:/Users/lx/IdeaProjects/xiedaimala-crawler/news", "root", "root");
+
         //待处理的链接池
-        List<String> linkpool = new ArrayList<>();
-        //已经处理的链接池
-        Set<String> processedlinks = new HashSet<>();
-        linkpool.add("https://sina.cn");
+        //从数据库加载即将处理的链接的代码
+        List<String> linkpool = loadUrlsFromDataBase(connection, "Select link from LINKS_TO_BE_PROCESSED");
+
 
         while (true) {
             if (linkpool.isEmpty()) {
                 break;
+            }
+
+            //从待处理池子中取一个来处理，
+            //处理完后从池子（包括数据库）中删除
+            String link = linkpool.remove(linkpool.size() - 1);
+
+            try(PreparedStatement statement = connection.prepareStatement("Delete from LINKS_TO_BE_PROCESSED where link = ?")){
+                statement.setString(1, link);
+                statement.executeUpdate();
+            }
+
+            if (isLinkProcessed(connection, link)) {
+                continue;
+            }
+
+            //我们只关心news.sina, 排除掉登录页面
+            if (isInterestingLink(link)) {
+                Document doc = httpGetAndParseHtml(link);
+                parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
+                storeIntoDatabaseIfItIsNewsPage(doc);
+                insertLinkIntoDatabase(connection, link, "INSERT INTO LINKS_ALREADY_PROCESSED (link) values (?)");
+            }
+        }
+    }
+
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+            insertLinkIntoDatabase(connection, href, "INSERT INTO LINKS_TO_BE_PROCESSED (link) values (?)");
+        }
+    }
+
+    //询问数据库，当前连接是否被处理过？
+    private static boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHERE LINK =?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
             }
             //ArrayList 从尾部删除更效率
             String link = linkpool.remove(linkpool.size() - 1);
@@ -70,6 +133,58 @@ public class Main {
             String html = EntityUtils.toString(entity1);
             return Jsoup.parse(html);
         }
+        return false;
+    }
+
+    private static void insertLinkIntoDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
+    //假如是新闻详情页面就存入数据库，否则不操作
+    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+        ArrayList<Element> articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                String title = articleTags.get(0).child(0).text();
+                System.out.println(title);
+            }
+        }
+    }
+
+    private static Document httpGetAndParseHtml(String link) throws IOException {
+        //要处理的新浪站内链接，感兴趣
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        System.out.println(link);
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        HttpGet httpGet = new HttpGet(link);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36");
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
+        }
+    }
+
+    private static boolean isInterestingLink(String link) {
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+    }
+
+    private static boolean isIndexPage(String link) {
+        return "https://sina.cn".equals(link);
+    }
+
+    private static boolean isNewsPage(String link) {
+        return link.contains("news.sina.cn");
+    }
+
+    private static boolean isNotLoginPage(String link) {
+        return !link.contains("passport.sina.cn");
     }
 
     private static boolean isInterestingLink(String link) {
